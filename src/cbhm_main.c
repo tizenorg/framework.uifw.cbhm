2fd2c887d6fc7b5584e52778405f99395942b23f
deasung.kim <deasung.kim@samsung.com>
2011-10-20 01:06:08 +0900

    change boilerplate(SLP)

--- src/cbhm_main.c
+++ src/cbhm_main.c
@@ -1,14 +1,21 @@
 /*
- * SLP2
- * Copyright (c) 2009 Samsung Electronics, Inc.
- * All rights reserved.
+ * Copyright (c) 2009 Samsung Electronics Co., Ltd All Rights Reserved
  *
- * This software is a confidential and proprietary information
- * of Samsung Electronics, Inc. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Samsung Electronics.
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * This software is the confidential and proprietary information of
+ * SAMSUNG ELECTRONICS ("Confidential Information"). You agree and
+ * acknowledge that this software is owned by Samsung and you shall
+ * not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered
+ * into with SAMSUNG ELECTRONICS.  SAMSUNG make no representations
+ * or warranties about the suitability of the software, either express
+ * or implied, including but not limited to the implied warranties of
+ * merchantability, fitness for a particular purpose, or non-infringement.
+ * SAMSUNG shall not be liable for any damages suffered by
+ * licensee arising out of or releated to this software.
  */
+
 
 #include "common.h"
 #include "cbhm_main.h"
